52d6a409355f4072b7f548b57da23bee51be3120
Camilo Avila <camilo.avil@gmail.com>
2025-03-19 14:29:40 -0500

    add nvim configuration

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    -- require("mason").setup {
+    --   ui = {
+    --     icons = {
+    --       package_installed = "✓",
+    --       package_pending = "➜",
+    --       package_uninstalled = "✗",
+    --     },
+    --   },
+    -- },
+    opts = require "configs.mason",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = require "configs.treesitter",
+  },
+  { "echasnovski/mini.nvim", version = false },
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+}
