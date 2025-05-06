dece377f8a4357390ddb1ba8012970c8f6580d76
Camilo Avila <camilo.avil@gmail.com>
2025-05-06 16:08:20 -0500

    feat(keymaps/options): Improve the nvim keymaps, add options and adjust stylua
    Updated the nvim key map, added in new key bindings for certain actions.
    
    Introduced new options in options.lua for enabling cursorline and adjusting conceal level.
    
    Adjusted indent width within stylua.toml from 2 to 4 spaces.
    
    No breaking changes introduced.

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- local o = vim.o
+-- o.relativenumber = true -- Habilita numeración relativa
+-- o.number = true -- Muestra el número absoluto en la línea actual
+vim.o.cursorlineopt = "both" -- to enable cursorline!
+vim.o.conceallevel = 1 -- So that I can see `` in markdown files Obsidian Plugin
