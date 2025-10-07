e4d6de28875ca4794e4f1a76141a6b068b8c8d0b
KmiAdmin <candres.avilg@gmail.com>
2025-10-06 21:20:20 -0500

    nvim: feat upgrade config acording main branch

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,38 @@
+return {
+  "stevearc/conform.nvim",
+  enable = false,
+  event = { "BufWritePre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- python = { "black" },
+        python = { "isort", "ruff_format" },
+        -- python = { { "isort", "black" } },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        svelte = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+      },
+      format_on_save = {
+        async = false,
+        timeout_ms = 750,
+        lsp_fallback = true,
+      },
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>mf", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 750,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
+}
--- nvim/.config/nvim/lua/plugins/venv-selector.lua
+++ nvim/.config/nvim/lua/plugins/venv-selector.lua
@@ -1,10 +1,10 @@
 return {
   "linux-cultist/venv-selector.nvim",
   dependencies = {
-    "neovim/nvim-lspconfig",
+    -- "neovim/nvim-lspconfig",
     -- "mfussenegger/nvim-dap",
     -- "mfussenegger/nvim-dap-python", --optional
-    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+    -- { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
   },
   lazy = false,
   opts = {
